ca472f15961b2252703f12cfd60bd78689da7a95
StampDanFan <claytonyu1@icloud.com>
2018-11-15 20:34:33 -0500

    Created Character and created canvas window

--- Game.py
+++ Game.py
@@ -1,1 +1,18 @@
+from tkinter import *
+import time
 
+class Player:
+  def __init__(self, canvas, color, x, y):
+    self.canvas = canvas
+    self.id = self.canvas.create_rectangle(0, 0, 50, 50, fill=color)
+    self.canvas.move(self.id, x * 50 - 50, y)
+
+tk = Tk()
+tk.title("Game")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+p = Player(canvas, "red", 1, 1)
